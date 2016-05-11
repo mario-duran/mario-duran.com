6ed1bfaa0f78387bd03ce0a05809d99d67a6c76f
Mario Duran <mduran@orinoco-systems.com>
2016-05-11 08:14:36 -0500

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mdComApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mdComApp
+ */
+angular.module('mdComApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mdComApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mdComApp
+ */
+angular.module('mdComApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
